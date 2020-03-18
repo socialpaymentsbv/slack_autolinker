a3623e406439f387a19456055644263993c974ff
Vanja Radovanović <elvanja@gmail.com>
2020-03-18 12:55:33 +0100

    Show version from mix.exs

--- lib/slack_autolinker/plug.ex
+++ lib/slack_autolinker/plug.ex
@@ -6,8 +6,10 @@ defmodule SlackAutolinker.Plug do
   def init(options), do: options
 
   def call(conn, _opts) do
+    {:ok, vsn} = :application.get_key(:slack_autolinker, :vsn)
+
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(200, Poison.encode!(%{version: "1.0.0-dev"}))
+    |> send_resp(200, Poison.encode!(%{version: List.to_string(vsn)}))
   end
 end
