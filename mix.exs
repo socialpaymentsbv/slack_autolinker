77300366ab6192876ad8d167f4171a302717f33f
Szymon Jeż <35540+szymon-jez@users.noreply.github.com>
2022-10-11 15:56:59 +0200

    Upgrade Elixir-Slack => upgrade Elixir and Heroku stack (#14)

--- mix.exs
+++ mix.exs
@@ -19,14 +19,14 @@ defmodule SlackAutolinker.Mixfile do
   defp deps do
     [
       {:cowboy, "~> 1.0.0"},
-      {:httpoison, "~> 0.9"},
+      {:httpoison, "~> 1.2"},
       {:plug, "~> 1.0"},
       {:poison, "~> 3.0"},
-      {:slack, "~> 0.9.3"},
+      {:slack, "~> 0.23.5"},
     ]
   end
 
   defp aliases do
-    ["test": ["test --no-start"]]
+    [test: ["test --no-start"]]
   end
 end
